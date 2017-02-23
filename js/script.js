d680984d3081bac2142bb38842c4e5ee7090703b
jeterian <jeterian@gmail.com>
2017-02-23 15:32:15 -0500

    Job Roles done
    Drop Down of “other” triggers a text box to appear. It does not appear
    when other job roles are selected.

--- js/script.js
+++ js/script.js
@@ -4,5 +4,31 @@
   Unit 3 - Interactive Form
 */
 
-// Focus for first box
+// Variables
+var jobRole = $("select#title");
+var designSel = document.getElementById("design");
+var colorSel = document.getElementById("color");
+
+// Focus for first box when page loads
 $(':input:enabled:visible:first').focus();
+
+// Job Role - reveal text field when "other" option is selected
+  // Hide text box initially
+  $("#other-title").hide();
+
+  // Changes whether the box appears depending on whether "Other" is selected
+  var jobRoleSelect = function () {
+    var jobRoles = jobRole.val();
+    if (jobRoles == "other") {
+        $("#other-title").slideDown(150).focus();
+    } else {
+        $("#other-title").slideUp(100).focus();
+    }
+}
+
+// calls function
+jobRole.change(function(e) {
+    jobRoleSelect();
+});
+
+// T-Shirt Info
